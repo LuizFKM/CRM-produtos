a9c780eff2d8f183eda2376167f4efe8815a7d96
luiz <luizfk.marinho@gmail.com>
2025-07-01 22:31:13 -0300

    add controller

---
+++ src/main/java/br/edu/compras02/controller/ProdutoController.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.edu.compras02.controller;
+
+import java.util.ArrayList;
+import br.edu.compras02.model.Produto;
+/**
+ *
+ * @author Aluno
+ */
+public class ProdutoController {
+private ArrayList<Produto> listaDeProdutos = new ArrayList<>();    
+}
