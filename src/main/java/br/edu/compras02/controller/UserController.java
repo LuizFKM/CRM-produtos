b1d3a7f1a47791dabaa67ac46a9c7d20ce381980
luiz <luizfk.marinho@gmail.com>
2025-07-04 20:29:03 -0300

    FEAT: Controller de produto

---
+++ src/main/java/br/edu/compras02/controller/UserController.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.edu.compras02.controller;
+
+import br.edu.compras02.model.Cliente;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Aluno
+ */
+public class UserController {
+    private ArrayList<Cliente> listaDeUsuarios = new ArrayList<>();
+}
